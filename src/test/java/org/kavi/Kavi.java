aa7bdbee4324d1263840f91e69b517b7c6ad857f
Parkavi95 <parkavicse104@gmil.com>
2022-09-18 08:36:45 -0700

    kavi methods

---
+++ src/test/java/org/kavi/Kavi.java
@@ -0,0 +1,17 @@
+package org.kavi;
+
+public class Kavi {
+
+private void method1() {
+	// TODO Auto-generated method stub
+
+}
+private void method2() {
+	// TODO Auto-generated method stub
+
+}
+private void method3() {
+	// TODO Auto-generated method stub
+
+}
+}
